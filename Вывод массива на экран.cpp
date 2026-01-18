ecc81b8c3c2c03e6ac39835e424df697f6de3f7d
lewykin1996 <lewykin1996@gmail.com>
2026-01-18 21:18:27 +0300

    Add files via upload

---
+++ Вывод массива на экран.cpp
@@ -0,0 +1,27 @@
+﻿// Вывод массива на экран.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	int arr[10] = { 5, 6, 7, 8, 60, 10, 11, 12, 13, 14 };
+	cout << '\n' << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << arr[i];
+
+		if (i < 9)
+		{
+			cout << ",";
+		}
+	}
+	cout << '\n' << endl;
+
+	return 0;
+}
